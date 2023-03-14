aa210a89ff9fae04d855d0074325e95b9b687397
Teedon <bola.com.ng123@gmail.com>
2023-03-14 10:40:07 +0100

    made webpage responsive

---
+++ Scripts/scripts.js
@@ -0,0 +1,28 @@
+document.querySelector(".python-button").addEventListener("click", function () {
+    document.querySelector(".drop1").style.display =
+        document.querySelector(".drop1").style.display === "flex" ? "none" : "flex";
+});
+document.querySelector(".excel-button").addEventListener("click", function () {
+    document.querySelector(".drop2").style.display =
+        document.querySelector(".drop2").style.display === "flex" ? "none" : "flex";
+});
+document.querySelector(".web-development-button").addEventListener("click", function () {
+    document.querySelector(".drop3").style.display =
+        document.querySelector(".drop3").style.display === "flex" ? "none" : "flex";
+});
+document.querySelector(".javascript-button").addEventListener("click", function () {
+    document.querySelector(".drop4").style.display =
+        document.querySelector(".drop4").style.display === "flex" ? "none" : "flex";
+});
+document.querySelector(".data-science-button").addEventListener("click", function () {
+    document.querySelector(".drop5").style.display =
+        document.querySelector(".drop5").style.display === "flex" ? "none" : "flex";
+});
+document.querySelector(".aws-certification-button").addEventListener("click", function () {
+    document.querySelector(".drop6").style.display =
+        document.querySelector(".drop6").style.display === "flex" ? "none" : "flex";
+});
+document.querySelector(".drawing-button").addEventListener("click", function () {
+    document.querySelector(".drop7").style.display =
+        document.querySelector(".drop7").style.display === "flex" ? "none" : "flex";
+});
